8bc3390a9f9ed28b488aa2de2ebe7835bd3cba89
Joaquim Juneo <jjsoprofissional@gmail.com>
2026-09-07 01:04:12 -0300

    Adequação dos nomes das páginas

--- index.php
+++ index.php
@@ -11,9 +11,9 @@ if (session_status() == PHP_SESSION_ACTIVE) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
+    <title>Home</title>
     <!-- <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&family=Inter:wght@400;500&display=swap" rel="stylesheet">
-    <title>Home</title>
     <style>
         div{
             text-align: center;
